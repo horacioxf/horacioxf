91102bde1d0f6e548a8d5fd2561c02190eaa7985
Horacio Flores <100793672+horacioxf@users.noreply.github.com>
2025-07-21 03:09:54 -0500

    Update WN10-AC-000010.ps1

--- STIGS Remediation/WN10-AC-000010.ps1
+++ STIGS Remediation/WN10-AC-000010.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that the number of allowed bad logon attempts is configured to 3.
 
 .NOTES
     Author          : Horacio Flores
